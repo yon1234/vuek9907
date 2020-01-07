45c0ef72576141ea8f68eedef529c72452b49ca0
zhang-yong001 <501173037@qq.com>
2020-01-07 11:46:25 +0800

    login第一次

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,17 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+#app{
+  width: 1118px;
+  margin: 0 auto;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+}
+li{
+  list-style: none;
 }
 </style>
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,62 +1,55 @@
 <template>
-    <div>
-首页
-<el-form ref="form" :model="sizeForm" label-width="80px" size="mini">
-  <el-form-item label="活动名称">
-    <el-input v-model="sizeForm.name"></el-input>
-  </el-form-item>
-  <el-form-item size="large">
-    <el-button type="primary" @click="onSubmit">立即创建</el-button>
-    <el-button>取消</el-button>
-  </el-form-item>
-</el-form>
-    </div>
+  <div>
+    首页
+    <el-form ref="form" :model="sizeForm" label-width="80px" size="mini">
+      <el-form-item label="用户名">
+        <el-input v-model="sizeForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="sizeForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item size="large">
+        <el-button type="primary" @click="onLogin">登录</el-button>
+        <el-button  @click="onSubmit">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-
-    },
-    data() {
-        return {
-sizeForm: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
-        };
-    },
-    computed: {
-
-    },
-    created() {
-
-    },
-    mounted() {
-
-    },
-    watch: {
-
-    },
-    methods: {
-         onSubmit() {
-        console.log('submit!');
+  props: {},
+  data() {
+    return {
+      sizeForm: {
+        name: "",
+        pass: "",
+        region: "",
+        delivery: false,
+        type: []
       }
-    
-
+    };
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {
+    onLogin() {
+      console.log("submit!");
+      console.log(this.sizeForm.name);
+      console.log(this.sizeForm.pass);
     },
-    components: {
+    onSubmit(){
+      this.sizeForm.name=''
+      this.sizeForm.pass=''
+    }
 
-    },
+  },
+  components: {}
 };
 </script>
 
 <style scoped >
-
 </style>
